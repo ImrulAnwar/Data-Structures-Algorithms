1497a64acf5ceaa29449227f338efa61eac8e27c
Imrul <imrulanwar836@gmail.com>
2022-07-14 17:47:40 +0600

    organized all the SortingAlgorithms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		int arr[] =new int[] {3, 4, 6, 1, -10, 6, 190, -98};
-		SortingAlgorithms.quickSort(arr);
+		SortingAlgorithms.insertionSort(arr, false);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]+" ");
 		}
--- src/com/company/algorithms/SortingAlgorithms.java
+++ src/com/company/algorithms/SortingAlgorithms.java
@@ -3,47 +3,69 @@ package com.company.algorithms;
 public class SortingAlgorithms {
 
 	public static void bubbleSort(int[] array, boolean isAscending) {
-		/*traverse the array. compare adjacent elements and swap them along the way
-		 it is stable*/
+		//traverse the array. compare adjacent elements and swap them along the way
+		//it is stable
+		if (isAscending) bubbleSortAscending(array);
+		else bubbleSortDescending(array);
+	}
+
+	private static void bubbleSortDescending(int[] array) {
 		int start = 0;
 		int end = array.length - 1;
-		while (end > start) {
-			// when end index is 1. it makes sense to terminate the loop
-			boolean isSwapped = false;
+		boolean isSwapped = false;
+		while (start < end) {
 			for (int i = start; i < end; i++) {
-				if (isAscending) {
-					if (array[i] > array[i + 1]) {
-						//difference between ascending and descending is just '>'/'<' this sign
-						Utils.swap(array, i, i + 1);
-						isSwapped = true;
-					}
-				} else {
-					if (array[i] < array[i + 1]) {
-						Utils.swap(array, i, i + 1);
-						isSwapped = true;
-					}
+				if (array[i] < array[i + 1]) {
+					Utils.swap(array, i, i + 1);
+					isSwapped = true;
 				}
 			}
-			end--;                                          // if the full array is traversed once without swapping that means, the array is sorted
-			if (!isSwapped) break;        // that's why I'm breaking the outer loop
+			if (!isSwapped) break;
+			end--;
+		}
+	}
 
+	private static void bubbleSortAscending(int[] array) {
+		int start = 0;
+		int end = array.length - 1;
+		boolean isSwapped = false;
+		while (start < end) {
+			for (int i = start; i < end; i++) {
+				if (array[i] > array[i + 1]) {
+					Utils.swap(array, i, i + 1);
+					isSwapped = true;
+				}
+			}
+			if (!isSwapped) break;
+			end--;
 		}
 	}
 
 	public static void selectionSort(int[] array, boolean isAscending) {
 		//select max or min element. Put it at its correct position
 		// it is not stable
+		if (isAscending) selectionSortAscending(array);
+		else selectionSortDescending(array);
+	}
+
+	private static void selectionSortDescending(int[] array) {
 		int start = 0;
 		int end = array.length - 1;
 		while (end > start) {
 			int index;
-			if (isAscending)
-				index = Utils.getMaxIndex(array, start, end);         // we are finding the max element
-			else index = Utils.getMinIndex(array, start, end);         // we are finding the min element
-			if (index != end) {
-				// if the max/min element is not at the end we will swap last element with selected element
-				Utils.swap(array, end, index);
-			}
+			index = Utils.getMinIndex(array, start, end);
+			if (index != end) Utils.swap(array, end, index);
+			end--;
+		}
+	}
+
+	private static void selectionSortAscending(int[] array) {
+		int start = 0;
+		int end = array.length - 1;
+		while (end > start) {
+			int index;
+			index = Utils.getMaxIndex(array, start, end);
+			if (index != end) Utils.swap(array, end, index);
 			end--;
 		}
 	}
@@ -52,20 +74,29 @@ public class SortingAlgorithms {
 		 /*we will take a sorted portion of an array and put element at their right position
 		in that sorted portion*/
 		// it is stable
+		if (isAscending) insertionSortAscending(array);
+		else insertionSortDescending(array);
+
+	}
+
+	private static void insertionSortDescending(int[] array) {
 		int start = 0;
 		while (start < array.length - 1) {                                              //this loop will run forward
 			for (int end = start + 1; end > 0; end--) {                       //this one will run backward
-				if (isAscending) {
-					if (array[end] < array[end - 1]) {
-						Utils.swap(array, end, end - 1);
-					} else
-						break;                   // if the new element is already at its correct position break the loop
-				} else {
-					if (array[end] > array[end - 1]) {
-						Utils.swap(array, end, end - 1);
-					} else
-						break;                   // if the new element is already at its correct position break the loop
-				}
+				if (array[end] > array[end - 1]) Utils.swap(array, end, end - 1);
+				else break;                   // if the new element is already at its correct position break the loop
+			}
+			start++;
+		}
+	}
+
+
+	private static void insertionSortAscending(int[] array) {
+		int start = 0;
+		while (start < array.length - 1) {                                              //this loop will run forward
+			for (int end = start + 1; end > 0; end--) {                       //this one will run backward
+				if (array[end] < array[end - 1]) Utils.swap(array, end, end - 1);
+				else break;                   // if the new element is already at its correct position break the loop
 			}
 			start++;
 		}
@@ -81,7 +112,6 @@ public class SortingAlgorithms {
 		leftIndicator = start;
 		rightIndicator = end;
 		int pivot = arr[end];
-
 		//partition
 		while (leftIndicator < rightIndicator) {
 			while (arr[leftIndicator] <= pivot && leftIndicator < rightIndicator) {
@@ -92,7 +122,6 @@ public class SortingAlgorithms {
 			}
 			Utils.swap(arr, leftIndicator, rightIndicator);
 		}
-
 		Utils.swap(arr, leftIndicator, end);
 
 		quickSort(arr, start, leftIndicator - 1);
