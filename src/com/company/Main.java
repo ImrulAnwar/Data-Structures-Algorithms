c7f8c131d396b6265edfbcdebd0bd6fcdf482aee
Imrul <imrulanwar836@gmail.com>
2022-07-30 11:27:32 +0600

    Checkpoint

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,70 @@ import com.company.data_structures.queue.QueueUsingArray;
 import com.company.data_structures.tree.BinaryTree;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		int arr[] =new int[] {3, 4, 6, 1, -10, 6, 190, -98};
-		SortingAlgorithms.quickSort(arr, false);
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]+" ");
+
+	public static int operation(int v1, int v2, Character op){
+		if(op.equals('+')){
+			return v1 + v2;
 		}
-		System.out.println();
+		else {
+			return v1 - v2;
+		}
+	}
+
+	public static int evalRPN(String tokens) {
+		Stack<Integer> st = new Stack<>();
+
+		for(int i = 0; i < tokens.length(); i++){
+			Character s = tokens.charAt(i);
+			if(s.equals('+') || s.equals('-')){
+				int v2 = st.pop();
+				int v1 = st.pop();
+				int tempVal = operation(v1,v2,s);
+				st.push(tempVal);
+			}else{
+				int x = Integer.parseInt(s.toString());
+				st.push(x);
+			}
+		}
+		int result = st.pop();
+		return result;
+	}
+
+	public static String toPostfix(String s){
+		Stack<Character> temp = new Stack<>();
+		Stack<Character> ans = new Stack<>();
+		Stack<Character> temp2 = new Stack<>();
+		temp.push('(');
+		for (int i=0;i<s.length();i++){
+			if (s.charAt(i) == '+' || s.charAt(i) == '-'){
+				temp.push(s.charAt(i));
+			} else if (s.charAt(i) == '(') {
+				while (!temp.empty() && temp.peek() != '(') {
+					ans.push(temp.peek());
+					temp.pop();
+				}
+				if (!temp.empty()) temp.pop();
+			}else {
+				ans.push(s.charAt(i));
+			}
+		}
+		String str = new String("");
+		while (!temp.empty()) {
+			temp2.push(temp.peek());
+			temp.pop();
+		}
+		while (!temp2.empty()) {
+			str += temp2.peek();
+			temp2.pop();
+		}
+		return str;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(evalRPN(toPostfix("(1+(4+5+2)-3)+(6+8)")));
 	}
 }
 
--- src/com/company/algorithms/SortingAlgorithms.java
+++ src/com/company/algorithms/SortingAlgorithms.java
@@ -9,22 +9,6 @@ public class SortingAlgorithms {
 		else bubbleSortDescending(array);
 	}
 
-	private static void bubbleSortDescending(int[] array) {
-		int start = 0;
-		int end = array.length - 1;
-		boolean isSwapped = false;
-		while (start < end) {
-			for (int i = start; i < end; i++) {
-				if (array[i] < array[i + 1]) {
-					Utils.swap(array, i, i + 1);
-					isSwapped = true;
-				}
-			}
-			if (!isSwapped) break;
-			end--;
-		}
-	}
-
 	private static void bubbleSortAscending(int[] array) {
 		int start = 0;
 		int end = array.length - 1;
@@ -41,22 +25,27 @@ public class SortingAlgorithms {
 		}
 	}
 
+	private static void bubbleSortDescending(int[] array) {
+		int start = 0;
+		int end = array.length - 1;
+		boolean isSwapped = false;
+		while (start < end) {
+			for (int i = start; i < end; i++) {
+				if (array[i] < array[i + 1]) {
+					Utils.swap(array, i, i + 1);
+					isSwapped = true;
+				}
+			}
+			if (!isSwapped) break;
+			end--;
+		}
+	}
+
 	public static void selectionSort(int[] array, boolean isAscending) {
 		//select max or min element. Put it at its correct position
 		// it is not stable
 		if (isAscending) selectionSortAscending(array);
 		else selectionSortDescending(array);
-	}
-
-	private static void selectionSortDescending(int[] array) {
-		int start = 0;
-		int end = array.length - 1;
-		while (end > start) {
-			int index;
-			index = Utils.getMinIndex(array, start, end);
-			if (index != end) Utils.swap(array, end, index);
-			end--;
-		}
 	}
 
 	private static void selectionSortAscending(int[] array) {
@@ -65,6 +54,17 @@ public class SortingAlgorithms {
 		while (end > start) {
 			int index;
 			index = Utils.getMaxIndex(array, start, end);
+			if (index != end) Utils.swap(array, end, index);
+			end--;
+		}
+	}
+
+	private static void selectionSortDescending(int[] array) {
+		int start = 0;
+		int end = array.length - 1;
+		while (end > start) {
+			int index;
+			index = Utils.getMinIndex(array, start, end);
 			if (index != end) Utils.swap(array, end, index);
 			end--;
 		}
@@ -79,6 +79,17 @@ public class SortingAlgorithms {
 
 	}
 
+	private static void insertionSortAscending(int[] array) {
+		int start = 0;
+		while (start < array.length - 1) {                                              //this loop will run forward
+			for (int end = start + 1; end > 0; end--) {                       //this one will run backward
+				if (array[end] < array[end - 1]) Utils.swap(array, end, end - 1);
+				else break;                   // if the new element is already at its correct position break the loop
+			}
+			start++;
+		}
+	}
+
 	private static void insertionSortDescending(int[] array) {
 		int start = 0;
 		while (start < array.length - 1) {                                              //this loop will run forward
@@ -90,17 +101,6 @@ public class SortingAlgorithms {
 		}
 	}
 
-
-	private static void insertionSortAscending(int[] array) {
-		int start = 0;
-		while (start < array.length - 1) {                                              //this loop will run forward
-			for (int end = start + 1; end > 0; end--) {                       //this one will run backward
-				if (array[end] < array[end - 1]) Utils.swap(array, end, end - 1);
-				else break;                   // if the new element is already at its correct position break the loop
-			}
-			start++;
-		}
-	}
 
 	public static void quickSort(int[] arr, boolean isAscending) {
 		/*select a pivot
@@ -115,47 +115,47 @@ public class SortingAlgorithms {
 
 	public static void quickSortAscending(int[] arr, int start, int end) {
 		if (start >= end) return;
-		int leftIndicator, rightIndicator;
-		leftIndicator = start;
-		rightIndicator = end;
+		int left, right;
+		left = start;
+		right = end;
 		int pivot = arr[end];
 		//partition
-		while (leftIndicator < rightIndicator) {
-			while (arr[leftIndicator] <= pivot && leftIndicator < rightIndicator) {
-				leftIndicator++;
+		while (left < right) {
+			while (arr[left] <= pivot && left < right) {
+				left++;
 			}
-			while (arr[rightIndicator] >= pivot && leftIndicator < rightIndicator) {
-				rightIndicator--;
+			while (arr[right] >= pivot && left < right) {
+				right--;
 			}
-			Utils.swap(arr, leftIndicator, rightIndicator);
+			Utils.swap(arr, left, right);
 		}
-		Utils.swap(arr, leftIndicator, end);
+		Utils.swap(arr, left, end);
 
-		quickSortAscending(arr, start, leftIndicator - 1);
-		quickSortAscending(arr, leftIndicator + 1, end);
+		quickSortAscending(arr, start, left - 1);
+		quickSortAscending(arr, left + 1, end);
 	}
 
 	public static void quickSortDescending(int[] arr, int start, int end) {
 		if (start >= end) return;
-		int leftIndicator, rightIndicator;
-		leftIndicator = start;
-		rightIndicator = end;
+		int left, right;
+		left = start;
+		right = end;
 		int pivot = arr[end];
 		//partition
-		while (leftIndicator < rightIndicator) {
+		while (left < right) {
 			//in descending only this condition will change
-			while (arr[leftIndicator] >= pivot && leftIndicator < rightIndicator) {
-				leftIndicator++;
+			while (arr[left] >= pivot && left < right) {
+				left++;
 			}
-			while (arr[rightIndicator] <= pivot && leftIndicator < rightIndicator) {
-				rightIndicator--;
+			while (arr[right] <= pivot && left < right) {
+				right--;
 			}
-			Utils.swap(arr, leftIndicator, rightIndicator);
+			Utils.swap(arr, left, right);
 		}
-		Utils.swap(arr, leftIndicator, end);
+		Utils.swap(arr, left, end);
 
-		quickSortDescending(arr, start, leftIndicator - 1);
-		quickSortDescending(arr, leftIndicator + 1, end);
+		quickSortDescending(arr, start, left - 1);
+		quickSortDescending(arr, left + 1, end);
 	}
 }
 
